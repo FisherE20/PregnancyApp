1da8204720cec1e00af4aa05dc216c3d48ca7f99
Erin Fisher <flybirdie85@gmail.com>
2020-10-13 14:05:30 -0700

    Added more pics and Registery page that is functional

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import SignIn from "./pages/SignIn";
 
 
+
 class App extends Component {
   render() {
     return (
---
+++ client/src/pages/Registeries.js
@@ -0,0 +1,45 @@
+import React from 'react';
+
+
+function Registeries() {
+   
+   
+    return (
+        <div class="grid-container">
+        <div class="grid-x grid-margin-x small-up-2 medium-up-3">
+          <div class="cell">
+            <div class="card">
+              <img src="./Images/Amazon.jpg"></img>
+              <div class="card-section">
+                <p>You'll find our registry by clicking on the button below.</p>
+                <a class="button primary" href="HTTPS://www.amazon.com">View</a>
+              </div>
+            </div>
+          </div>
+          <div class="cell">
+            <div class="card">
+              <img src="./Images/BuyBuyBaby.jpg"></img>
+              <div class="card-section">
+                <p>You'll find our registry by clicking on the button below.</p>
+                <a class="button primary" href="HTTPs://www.BuyBuyBaby.com">View</a>
+              </div>
+            </div>
+          </div>
+          <div class="cell">
+            <div class="card">
+              <img src="./Images/Target.jpg"></img>
+              <div class="card-section">
+                <p>You'll find our registry by clicking on the button below.</p>
+                <a class="button primary" href="HTTPS://www.target.com">View</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    );
+
+    
+}
+
+export default Registeries;
