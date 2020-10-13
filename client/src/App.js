e3ecb60f5e15a016321f134620ff846bc774e189
Erin Fisher <flybirdie85@gmail.com>
2020-10-13 15:15:34 -0700

    added to SignUp form and started the journal portion of the app

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 
 
@@ -10,7 +10,7 @@ class App extends Component {
         <div className="App-header">
           <h2>Welcome To My Pregnancy Journal</h2>
         </div>
-        <SignIn />
+        <SignUp />
       </div>
     );
   }
--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -1,22 +1,26 @@
 import React, { useState } from "react";
-import Container from "../../components/Container";
-import Col from "../../components/Col";
-import Row from "../../components/Row";
+import Container from "../components/Container/index";
+import Col from "../components/Col/index";
+import Row from "../components/Row/index";
 
 function Signup() {
   const [username, setUsername] = useState();
   const [password, setPassword] = useState();
+  const [duedate, setDuedate] = useState();
+  const [nickname, setNickname] = useState();
 
   const handleSubmit = e => {
     e.preventDefault();
     console.log("username is " + username);
     console.log("password is " + password);
+    console.log("duedate is " + duedate);
+    console.log("nickname is " + nickname);
   };
 
   return (
     <div>
       <div className="mt-4">
-        <h2>Welcome to My Pregnancy Journal</h2>
+        <h2>Sign Up</h2>
       </div>
       <form onSubmit={handleSubmit}>
         <Container className="mt-3 px-5">
@@ -39,6 +43,28 @@ function Signup() {
                 placeholder="Password"
                 name="password"
                 onChange={e => setPassword(e.target.value)}
+              />
+            </Col>
+          </Row>
+          <Row className="form-group">
+            <Col size="12">
+              <input
+                className="form-control"
+                type="duedate"
+                placeholder="Baby's estimated Due Date"
+                name="duedate"
+                onChange={e => setDuedate(e.target.value)}
+              />
+            </Col>
+          </Row>
+          <Row className="form-group">
+            <Col size="12">
+              <input
+                className="form-control"
+                type="nickname"
+                placeholder="Baby's Nickname"
+                name="nickname"
+                onChange={e => setNickname(e.target.value)}
               />
             </Col>
           </Row>
